4f4a95796b47d526f1d1c6e0450f3a1d03ecd80d
raomaiping <2582395486@qq.com>
2020-02-07 23:00:32 +0800

    解决用户授权

--- pages/me/index.js
+++ pages/me/index.js
@@ -1,3 +1,7 @@
+//获取应用实例
+const app = getApp();
+// console.log(app);
+
 // pages/me/index.js
 Page({
 
@@ -15,7 +19,21 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    if(app.globalData.userInfo){
+      this.setData({
+        userInfo:app.globalData.userInfo,
+        hasUserInfo:true
+      })
+    }else if(this.data.canIUse){
+      //由于getUserInfo是网络请求，可能会在Page.onLoad之后才返回
+      //为了防止这种情况发生，所以此处加了callback
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo:res.userInfo,
+          hasUserInfo:true
+        })
+      }
+    }
   },
 
   //实现获取用户信息的方法
@@ -25,6 +43,5 @@ Page({
       userInfo:e.detail.userInfo,
       hasUserInfo:true
     })
-    console.log(this)
   }
 })
