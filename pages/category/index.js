082e1b93b265d4fdccf69a7c57137b55c9188b97
raomaiping <2582395486@qq.com>
2020-02-08 21:56:22 +0800

    展示左侧导航信息

--- pages/category/index.js
+++ pages/category/index.js
@@ -8,7 +8,8 @@ Page({
    */
   data: {
     navLeftItems:[],
-    navRightItems:[]
+    navRightItems:[],
+    curIndex:0
   },
 
   /**
@@ -34,53 +35,14 @@ Page({
       }
     })
   },
+  switchRightTab(e){
+    // console.log(111);
+    // console.log(e);
+    let index = parseInt(e.currentTarget.dataset.index);
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+    this.setData({
+      curIndex:index
+    })
   }
+ 
 })
