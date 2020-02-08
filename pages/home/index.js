bf908af3de0e84eb14933d2a089e3113862f9d52
raomaiping <2582395486@qq.com>
2020-02-08 21:09:37 +0800

    首页展示（搜索和轮播）

--- pages/home/index.js
+++ pages/home/index.js
@@ -11,7 +11,12 @@ Page({
     swipers:[],
     logos:[],
     quicks:[],
-    pageRow:[]
+    pageRow:[],
+    indicatorDots:true,
+    vertical:false,
+    autoplay:true,
+    interval:3000,
+    duration:500
   },
 
   /**
