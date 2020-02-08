5623076ea27cc3e7a56896e2629a3e8d55e5d8a3
raomaiping <2582395486@qq.com>
2020-02-08 21:29:46 +0800

    获取分类数据

--- pages/category/index.js
+++ pages/category/index.js
@@ -1,3 +1,5 @@
+const interfaces = require("../../utils/urlconfing.js");
+
 // pages/category/index.js
 Page({
 
@@ -5,14 +7,32 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    navLeftItems:[],
+    navRightItems:[]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    const self = this;
+    wx.showLoading({
+      title: '加载中...',
+    })
+    wx.request({
+      url: interfaces.productions,
+      header:{
+        "content-type":"application/json"
+      },
+      success(res){
+        // console.log(res.data);
+        self.setData({
+          navLeftItems:res.data.navLeftItems,
+          navRightItems:res.data.navRightItems
+        })
+        wx.hideLoading();
+      }
+    })
   },
 
   /**
