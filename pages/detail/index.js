4f6acc9c1cdbf6d087af3a9ab83ad30298b5aea2
raomaiping <2582395486@qq.com>
2020-03-05 21:46:57 +0800

    加入购物车本地存储

--- pages/detail/index.js
+++ pages/detail/index.js
@@ -76,7 +76,55 @@ Page({
   },
 
   addCart(){
-    console.log("加入购物车")
+    // console.log("加入购物车")
+    let self = this;
+    wx.getStorage({
+      key: 'cartInfo',
+      //查到cartInfo这个数据了，判断数组中是否拥有当前添加的商品
+      success(res) {
+        // console.log(res)
+        let cartArray = res.data;
+        //拿到现在添加的商品对象
+        let partData = self.data.partData;
+        let isExit = false;// 判断数组是否存在该商品
+
+        //匹配本地数组和当前对象的id是否一致
+        cartArray.forEach(cart =>{
+          if(cart.id == partData.id){
+            isExit = true;
+            cart.total += self.data.partData.count;
+            wx.setStorage({
+              key: 'cartInfo',
+              data: cartArray,
+            })
+          }
+        })
+        if(!isExit){ //不存在商品
+          partData.total = self.data.partData.count;
+          cartArray.push(partData);
+          wx.setStorage({
+            key: 'cartInfo',
+            data: cartArray,
+          })
+        }
+      },
+      fail(){
+        let partData = self.data.partData;
+        partData.total = self.data.partData.count;
+        // console.log(partData);
+        let cartArray = [];
+        cartArray.push(partData);
+        wx.setStorage({
+          key: 'cartInfo',
+          data: cartArray,
+        })
+      }
+    }),
+    wx.showToast({
+      title: '加入购物车成功',
+      icon:"success",
+      duration:3000
+    })
   },
   /**
    * 生命周期函数--监听页面初次渲染完成
